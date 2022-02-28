d2a7e55bfa50fab5ff6862a81c59db77df044741
Aanchal-Ratta <aanchalrattan22@gmai.com>
2022-02-28 12:59:01 +0530

    Part 2 Resume-Builder

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux'
+import rootReducer from './redux/reducers/rootReducer'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { Provider } from 'react-redux';
+
+const reduxStore = createStore(rootReducer, composeWithDevTools()) 
+
+
+ReactDOM.render(
+
+  <BrowserRouter>
+    <Provider store={reduxStore}>
+      <App />
+    </Provider>
+
+  </BrowserRouter>
+  ,
+  document.getElementById('root')
+);
